a95b131e4ca9f949bd87ed474f732cae671536df
wskdxsxz101 <19886666037@163.com>
2026-08-05 16:41:03 +0800

    Add files via upload

---
+++ work2.py
@@ -0,0 +1,14 @@
+name = input("confirm your name:")
+print("hello", name)
+
+enter_hour = int(input("hour:"))
+enter_rate = int(input("rate:"))
+print("pay:", enter_hour*enter_rate)
+
+width = 17
+height = 12.0
+print(width // 2)
+print(width / 2.0)
+print(height / 3.0)
+print(1 + 2 * 5)
+
