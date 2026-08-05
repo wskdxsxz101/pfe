9bcfa0644c081e68e28f7844a10616675a376f14
wskdxsxz101 <19886666037@163.com>
2026-08-05 16:30:29 +0800

    Add files via upload

---
+++ introduction(2).py
@@ -0,0 +1,29 @@
+hours = 35.0
+rate = 12.50
+pay = hours * rate
+print(pay)
+
+x = 0.6
+x = 3.9 * x * ( 1 - x )
+
+xx = 2
+xx = xx + 2
+print(xx)
+yy = xx * 2
+print(yy)
+zz = yy ** 2
+print(zz)
+jj = zz % 3
+print(jj)
+
+"""eee = 'hellp' + 'there'
+eee = eee + 1"""
+
+print(float(99.0) + 100) # float和int一起会转为float
+
+name = input("what's  your name? ")
+print("welcome",name)
+
+Ef = int(input("what's your floor? "))
+Uf = Ef + 1
+print(Uf)
