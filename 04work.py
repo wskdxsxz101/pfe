aa8c39b126e34a489c9cc732656c69094da02247
wskdxsxz101 <19886666037@163.com>
2026-08-06 14:28:40 +0800

    Update 04work.py

--- 04work.py
+++ 04work.py
@@ -1,14 +1,14 @@
-# def computepay_hours_rate():
-#     if enter_hours <= 40:
-#         print("pay:", enter_hours * enter_rate)
-#     else:
-#         print("pay:", enter_rate * 40 + (enter_hours - 40) * 1.5 * enter_rate)
-# try:
-#     enter_hours = int(input("workhours:"))
-#     enter_rate = int(input("rate:")) 
-#     computepay_hours_rate()
-# except:
-#     print("Error, please enter numeric input") # 典型为用而用的函数，没有复用的价值
+def computepay_hours_rate():
+    if enter_hours <= 40:
+        print("pay:", enter_hours * enter_rate)
+    else:
+        print("pay:", enter_rate * 40 + (enter_hours - 40) * 1.5 * enter_rate)
+try:
+    enter_hours = int(input("workhours:"))
+    enter_rate = int(input("rate:")) 
+    computepay_hours_rate()
+except:
+    print("Error, please enter numeric input") # 典型为用而用的函数，没有复用的价值
 
 def core():
     if score < 6.0:
